15a35a8b68cd32a187c9cccf1a1929d862758845
Yingchuan Hu <yh537@cornell.edu>
2013-09-18 15:43:12 -0400

    Cylinder debugged and tested
    Cylinder done.

--- src/ray/surface/Cylinder.java
+++ src/ray/surface/Cylinder.java
@@ -1,6 +1,8 @@
 
 package ray.surface;
 
+import java.util.Arrays;
+
 import ray.IntersectionRecord;
 import ray.Ray;
 import ray.math.Point3;
@@ -9,7 +11,7 @@ import ray.math.Vector3;
 /**
  * Represents a cylinder as a center, a radius and a height
  *
- * @author Yingchuan Hu
+ * @author Yingchuan Hu   yh537@cornell.edu
  */
 public class Cylinder extends Surface {
   
@@ -39,73 +41,81 @@ public class Cylinder extends Surface {
    */
   public boolean intersect(IntersectionRecord outRecord, Ray rayIn) {
     // TODO: fill in this function.
-	// Get point p and direction d
-    Point3 p = new Point3();
-    p.set(rayIn.origin);
-    Vector3 d = new Vector3();
-    d.set(rayIn.direction);
+	// Get vector op and direction d
+    Vector3 op = new Vector3();
+    op.sub(rayIn.origin, center);
+    Vector3 d = rayIn.direction;    
     
     // r(t) = p + td && |x|^2+|y|^2 = radius^2 && -height/2 < z < height/2
     // Assume that the length of the cylinder is infinite
     // so that we can treat it similar to sphere in the xy plane
     // limitation on z would be added later
     double dd = d.x * d.x + d.y * d.y;
-    double pp = p.x * p.x + p.y * p.y;
-    double dp = d.x * p.x + d.y * p.y;
-    double delta = dp * dp - dd * (pp - radius * radius);
-    
+    double dop = d.x * op.x + d.y * op.y;
+    double opop = op.x * op.x + op.y * op.y;
+    double delta = dop * dop - dd * (opop - radius * radius);
     // Point out of cylinder
-    if (delta < 0 )
+    if (delta < 0)
     	return false;
     
     // Intersect with side plane
-    double t_side = Math.min(-dp + Math.sqrt(delta), -dp - Math.sqrt(delta));
-    // Intersect with top cap, Infinity if p.z == 0
-    double t_top = (height / 2.0 - p.z) / p.z;
-    // Intersect with bottom cap, Infinity if p.z == 0
-    double t_bot = (-height / 2.0 - p.z) / p.z;
-    
-    // We should find the minimum t satisfying our requirements
-    double t_min = Math.min(Math.min(t_side, t_top), t_bot);
-    double tmpMinT = Double.MAX_VALUE;
-    IntersectionRecord tmpRecord = new IntersectionRecord();
-    if (t_min == t_side)
+    double t_side = Math.min((-dop + Math.sqrt(delta)) / dd, (-dop - Math.sqrt(delta)) / dd);
+    // Intersect with top cap, Infinity if d.z == 0
+    double t_top = (height / 2.0 - op.z) / d.z;
+    // Intersect with bottom cap, Infinity if d.z == 0
+    double t_bot = (-height / 2.0 - op.z) / d.z;
+    double[] tList = {t_side, t_top, t_bot};
+    Arrays.sort(tList);
+    Double t_min = Double.MAX_VALUE;
+    // Loop from the minimum t to find the minimum t that satisfies all requirements
+    for (double t : tList)
     {
-        // p + td
-        rayIn.evaluate(tmpRecord.location, t_side);
-        // Limitation added
-        if (Math.abs(tmpRecord.location.z - center.z) < height / 2.0)
-        {
-        	outRecord.normal.set(tmpRecord.location.x - center.x, tmpRecord.location.y - center.y, 0);
-        	outRecord.normal.normalize();
-        	tmpMinT = t_min;
-        }
-    }
-    // Top or bottom cap
-    else
-    {
-    	if (Math.pow(tmpRecord.location.x - center.x, 2) + Math.pow(tmpRecord.location.y - center.y, 2) <= radius * radius)
+    	// Calculate the possible intersection point by p + td
+    	Vector3 tmpVec = new Vector3();
+    	tmpVec.scaleAdd(t, d);
+    	Point3 tmpLoc = new Point3();
+    	tmpLoc.add(rayIn.origin, tmpVec);
+    	// If intersect with side surface
+    	if (t == t_side)
     	{
-    		if (t_min == t_top)
-    			outRecord.normal.set(0,0,1);
-    		else if (t_min == t_bot)
-    			outRecord.normal.set(0,0,-1);
-    		tmpMinT = t_min;
+    		if (Math.abs(tmpLoc.z - center.z) < height / 2.0)
+    		{
+    			outRecord.normal.set(tmpLoc.x - center.x, tmpLoc.y - center.y, 0);
+    			outRecord.normal.normalize();
+    			t_min = t;
+    			break;
+    		}
+    	}
+    	else
+    	{
+    		// Must be inside circle if seen from the x-y plane
+    		if (Math.pow(tmpLoc.x - center.x, 2) + Math.pow(tmpLoc.y - center.y, 2) - radius * radius <= 0)
+    		{
+    			// If intersect with top
+    			if (t == t_top)
+    				outRecord.normal.set(0, 0, 1);
+    			// If intersect with bottom
+    			else if (t == t_bot)
+    				outRecord.normal.set(0, 0, -1);
+    			t_min = t;
+    			break;
+    		}
     	}
     }
     // Check whether between ray start and end point or error occurs
-    if (tmpMinT == Double.MAX_VALUE || tmpMinT < rayIn.start || tmpMinT > rayIn.end)
+    if (t_min == Double.MAX_VALUE || t_min < rayIn.start || t_min > rayIn.end)
     	return false;
     
     // Output
-    outRecord.t = tmpMinT;
-    Point3 loc = new Point3();
-    rayIn.evaluate(loc, tmpMinT);
-    outRecord.location.set(loc);
-    outRecord.surface = this;
+    outRecord.t = t_min;
     
+    Vector3 scaleD = new Vector3();
+	scaleD.scaleAdd(t_min, d);
+	outRecord.location.add(rayIn.origin, scaleD);
+    
+	outRecord.surface = this;
+    rayIn.end = t_min;
     return true;
-
   }
   
   /**
